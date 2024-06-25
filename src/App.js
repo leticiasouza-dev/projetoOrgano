aa807eaa8fcfd4eb6d59e17d411f027ac44ab022
leticiaSouzaDev <lec667121@gmail.com>
2024-06-25 15:58:05 -0300

    feat: adicionando os colaboradores ao meu array  e passando por props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Formulario from './components/Formulario';
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([]) // meu estado receberá um array
+
   return (
     <div className="App">
       <Banner />
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={colaborador => setColaboradores(...colaboradores, colaborador)}/>{/*passando paro o meu set que vou receber os antigos colaboradores e os novos  */}
+
     </div>
+    
   );
 }
 
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,7 +4,7 @@ import Botao from '../Botao';
 import './Formulario.css';
 import { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = (props) => {
     const times = [
         'Programação',
         'Front-end',
@@ -22,7 +22,12 @@ const Formulario = () => {
 
     const aoSalvar = (evento) =>{ 
         evento.preventDefault();
-        console.log('Form foi submetido =>', nome, cargo, imagem, time)
+        props.aoColaboradorCadastrado({ // pequeno objeto sendo passado como props
+            nome, 
+            cargo, 
+            imagem, 
+            time
+        })
     }
 
     return(
